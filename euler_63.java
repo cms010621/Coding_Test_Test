a76e656d5a9749b223f1af93f781d8a84dfc4261
cms010621 <cms010621@gmail.com>
2020-06-25 10:17:40 +0900

    euler_63

---
+++ euler_63.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+
+public class euler_63 {
+    public static void main(String args[]) {
+        double count = 0;
+        for(int i=1;i<10;i++) { 
+            for(int j=1;j<100000000;j++) {
+                BigInteger first = BigInteger.valueOf(i);
+                BigInteger n = first.pow(j);
+                String s = n.toString();
+                if(s.length() < j) {
+                    break;
+                }
+                if(s.length() == j) { //길이같을때
+                    System.out.println("밑 수 : " +i+  "위 수 : "+j+"  숫자 : "+s);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
