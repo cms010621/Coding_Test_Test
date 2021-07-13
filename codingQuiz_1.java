85dabe66e603f2ce8cdede3397adbe0138af7c2c
cms010621 <cms010621@gmail.com>
2021-07-13 16:43:35 +0900

    codingQuize_1 논리오류

--- codingQuiz_1.java
+++ codingQuiz_1.java
@@ -1,13 +1,20 @@
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class codingQuiz_1 {
     public static void main(String[] args) {
         int calNum = 0; //곱한 값을 더하는 변수
         int presNum = 0; //현재 수 == i
-        int count = 0;
+        int count = 0; //반복 횟수
+        int totCount = 0; //행복수의 총 개수
+        int totVal = 0; //행복수의 총 합
         String arr[];
-        for(presNum=2;presNum<3; presNum++) {
+        Scanner Sc = new Scanner(System.in);
+        int num = Sc.nextInt();
+        
+        for(presNum=1;presNum<=num; presNum++) {
+            count = 0;
             arr = Integer.toString(presNum).split("");
             while(true) {
                 calNum = 0;
@@ -15,15 +22,20 @@ public class codingQuiz_1 {
                     calNum += Math.pow(Integer.parseInt(arr[i]), 2);
                 }
                 arr = Integer.toString(calNum).split("");
-                // System.out.println("calNum : "+calNum);
-                // System.out.println("presNum : "+presNum);
-                if(calNum == presNum || calNum == 1) 
+                if(calNum == presNum || calNum == 1 || count == 100)  {
+                    if(calNum == 1) {
+                        totCount +=1;
+                        totVal += presNum;
+                    }
                     break;
+                }
                 count ++;
             }
-            System.out.println("calNum : "+calNum);
-            System.out.println("presNum : "+presNum);
         }
+        System.out.println("totCount : "+totCount);
+        System.out.println("totVal : "+totVal);
+        System.out.println("total : "+BigInteger.valueOf(totCount*totVal));
+        //1622531837
         
     }
 }
