14c3402ff2f444f8f83d89b655520962c02291f3
cms010621 <cms010621@gmail.com>
2021-07-14 09:18:37 +0900

    codingQuiz 1번 92번 풀이

--- codingQuiz_1_1.java
+++ codingQuiz_1_1.java
@@ -1,16 +1,18 @@
+import java.math.BigInteger;
+
 public class codingQuiz_1_1 {
     public static void main(String args[]) {
-        int count = 0;
-        int value = 0;
-        for(int i=1;i<10000;i++) {
+        BigInteger count = BigInteger.ZERO;
+        BigInteger value = BigInteger.ZERO;
+        for(int i=1;i<=9999;i++) {
             if(Squared(i) == 1) {
-                count++;
-                value+=i;
+                count = count.add(BigInteger.ONE);
+                value = value.add(BigInteger.valueOf(i));
             }
         }
-        System.out.println("1 :  "+count+" 개");
-        System.out.println("총합 : "+value);
-        System.out.println(value*count);
+        // System.out.println("1 :  "+count+" 개");
+        // System.out.println("총합 : "+value);
+        System.out.println("1 ~ 9999범위의 행복 수는 "+count+"개이고 총합은 "+count.multiply(value)+"입니다.");
     }
     public static int Squared(int number) {
         String arr[] = String.valueOf(number).split("");
