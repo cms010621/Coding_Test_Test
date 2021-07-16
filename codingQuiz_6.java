e5a0e3fcec9f4d2dab5a882d0d4c9696408573e0
cms010621 <cms010621@gmail.com>
2021-07-16 10:33:14 +0900

    codingQuiz_6.java 간결화

--- codingQuiz_6.java
+++ codingQuiz_6.java
@@ -7,24 +7,20 @@ public class codingQuiz_6 {
         Fibo.add(BigInteger.ONE);
         Fibo.add(BigInteger.valueOf(2));
         BigInteger cal = BigInteger.ZERO;
+        BigInteger tot = BigInteger.ZERO;
         int count = 2;
 
         while(true) {
             cal = Fibo.get(count-2).add(Fibo.get(count-1));
-            // System.out.println(cal);
+            if(cal.compareTo(BigInteger.valueOf(12345678999L))==1 && cal.compareTo(BigInteger.valueOf(99987654321L))==-1) 
+                tot = tot.add(cal);
+            
             Fibo.add(cal);
-
             count++;
-            if(cal.compareTo(BigInteger.valueOf(100000000000L))==1) break;
-        }
-        cal = BigInteger.ZERO;
-        for(int i=0;i<Fibo.size();i++) {
-            if(Fibo.get(i).compareTo(BigInteger.valueOf(12345678999L))==1 && Fibo.get(i).compareTo(BigInteger.valueOf(99987654321L))==-1) {
-                cal = cal.add(Fibo.get(i));
-            }
+            if(cal.compareTo(BigInteger.valueOf(99987654321L))==1) break;
         }
 
-        System.out.println(cal);
+        System.out.println(tot);
 
     }
 }
